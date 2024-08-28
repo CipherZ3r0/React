825ceb7d3bf5878a4bc5b46cc6958a60ef0c96a1
Cyph3rZ3r0 <nilax.pln@gmail.com>
2024-08-28 22:39:31 +0530

    day 2 of react

---
+++ 01_react_basics/src/Chai.js
@@ -0,0 +1,9 @@
+//Starting filename with Capitle latter is considered Good practice
+
+function Chai () {
+    return (
+        <h2>hello Chai</h2>
+    )
+}
+
+export default Chai
---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Chai from "./chai"
+
+function App() {
+
+  return (
+
+    //we wrote everything in <> </> because jsx only returns only one element 
+    // everything inside  <> </> will go as one element  
+
+
+    <>
+    <Chai/>
+    <h1>hello Vite</h1>
+    </>
+  )
+}
+
+export default App
